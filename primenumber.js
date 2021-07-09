113e52880690eab8ba913e7fb2c880b670f1ff88
amirulameen <85555727+amirulameen@users.noreply.github.com>
2021-07-09 14:13:16 +0800

    Create primenumber.js

---
+++ primenumber.js
@@ -0,0 +1,13 @@
+function isPrime(num){
+  for (var i = 2; i < num; i++){
+    if (num % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+for (var i = 1; i < 1000; i++){
+  if (isPrime(i)){
+    console.log(i)
+  }
+}
